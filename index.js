649f889156299e5be60ae9a9d5287eb8578c43ae
Fyphen <fyphensub@gmail.com>
2024-09-25 09:57:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Node } = require('./src/Node');
+const { Player } = require('./src/Player');
 const { TsumiInstance } = require('./src/TsumiInstance');
 
 module.exports = { Node, TsumiInstance };
